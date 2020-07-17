4a69d300fa63d0de8fb9b59d3ef9f909e6c97338
Ciprian Burca <ciprian.burca@outlook.com>
2020-07-17 12:27:54 +0300

    Display downloadable versions in TeamCity tools section

---
+++ advinst-server/src/main/java/jetbrains/buildServer/advinst/server/AdvinstDownloadableToolVersion.java
@@ -0,0 +1,52 @@
+package jetbrains.buildServer.advinst.server;
+
+import jetbrains.buildServer.advinst.common.AdvinstConstants;
+import jetbrains.buildServer.tools.ToolType;
+import jetbrains.buildServer.tools.available.DownloadableToolVersion;
+import org.jetbrains.annotations.NotNull;
+
+
+
+public class AdvinstDownloadableToolVersion implements DownloadableToolVersion {
+  private String version;
+
+  AdvinstDownloadableToolVersion(@NotNull String version) {
+    this.version = version;
+  }
+
+  @NotNull
+  @Override
+  public String getDestinationFileName() {
+    return String.format(AdvinstConstants.ADVINST_VERSION_FILE_NAME, version);
+  }
+
+  @NotNull
+  @Override
+  public String getDownloadUrl() {
+    return String.format(AdvinstConstants.ADVINST_VERSION_URL, version);
+  }
+
+  @NotNull
+  @Override
+  public ToolType getType() {
+    return AdvinstToolProvider.ADVINST_TOOL_TYPE;
+  }
+
+  @NotNull
+  @Override
+  public String getDisplayName() {
+    return getId();
+  }
+
+  @NotNull
+  @Override
+  public String getId() {
+    return "Advinst-" + version;
+  }
+
+  @NotNull
+  @Override
+  public String getVersion() {
+    return version;
+  }
+}
