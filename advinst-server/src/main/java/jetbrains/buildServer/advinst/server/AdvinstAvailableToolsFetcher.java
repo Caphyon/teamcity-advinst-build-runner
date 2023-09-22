59ea9ad4535423fecb892aab4a1d744325eeb9e2
Ciprian Burca <ciprian.burca@outlook.com>
2023-09-22 11:11:00 +0300

    Only offer valid versions for tool installation

--- advinst-server/src/main/java/jetbrains/buildServer/advinst/server/AdvinstAvailableToolsFetcher.java
+++ advinst-server/src/main/java/jetbrains/buildServer/advinst/server/AdvinstAvailableToolsFetcher.java
@@ -1,17 +1,16 @@
 package jetbrains.buildServer.advinst.server;
 
 import jetbrains.buildServer.advinst.common.AdvinstConstants;
+import jetbrains.buildServer.advinst.common.AdvinstException;
+import jetbrains.buildServer.advinst.common.AdvinstVersions;
 import jetbrains.buildServer.tools.available.AvailableToolsFetcher;
 import jetbrains.buildServer.tools.available.FetchAvailableToolsResult;
 
-import org.ini4j.Profile;
-import org.ini4j.Wini;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.stream.Collectors;
+
 
 public class AdvinstAvailableToolsFetcher implements AvailableToolsFetcher {
 
@@ -19,16 +18,13 @@ public class AdvinstAvailableToolsFetcher implements AvailableToolsFetcher {
   @Override
   public FetchAvailableToolsResult fetchAvailable() {
     try {
-      final URL updatesIniUrl = new URL("https://www.advancedinstaller.com/downloads/updates.ini");
-      Wini updatesIni = new Wini(updatesIniUrl);
-      final Collection<AdvinstDownloadableToolVersion> advinstVersions = new ArrayList<>();
-      for (Profile.Section section : updatesIni.values()) {
-        advinstVersions.add(new AdvinstDownloadableToolVersion(section.get("ProductVersion")));
-      }
+      final Collection<AdvinstDownloadableToolVersion> advinstVersions = AdvinstVersions.getAllowedVersions().stream()
+          .map(v -> new AdvinstDownloadableToolVersion(v)).collect(Collectors.toList());
       return FetchAvailableToolsResult.createSuccessful(advinstVersions);
-    } catch (IOException e) {
+    } catch (AdvinstException e) {
       return FetchAvailableToolsResult.createError(
-          "Failed to fetch available Advanced Installer versions from " + AdvinstConstants.ADVINST_TOOL_VERSIONS_URL, e);
+          "Failed to fetch available Advanced Installer versions from " + AdvinstConstants.ADVINST_TOOL_VERSIONS_URL,
+          e);
     }
   }
 }
